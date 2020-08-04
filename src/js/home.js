e56c402b6d8a43cac18aaed5671ac10fa5d79f9f
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-08-04 11:06:08 -0700

    Templates

--- src/js/home.js
+++ src/js/home.js
@@ -1,19 +1,53 @@
-const $actionContainer = document.querySelector('#action')
-const $adventureContainer = document.getElementById('#adventure')
-const $animationContainer = document.getElementById('#animation')
+(async function load() {
+    async function getData(url) {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    }
+    const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
+    const adventureList = await getData('https://yts.mx/api/v2/list_movies.json?genre=adventure')
+    const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
 
-const $home = document.getElementById('#home')
+    function videoItemTemplate(movie) {
+        return (
+            `< div class="primaryPlaylistItem" >
+                <div class="primaryPlaylistItem-image">
+                    <img src="${movie.medium_cover_image}">
+                </div>
+                <h4 class="primaryPlaylistItem-title">
+                    ${movie.title}
+                </h4>
+            </div>`
+        )
+    }
 
-//-------------------------------------------------------- Formulario
-const $featuringContainer = document.getElementById('#featuring')
-const $form = document.getElementById('#form')
+    actionList.data.movies.forEach((movie) => {
+        const HTMLString = videoItemTemplate(movie);
+        console.log(HTMLString)
+    });
 
-//-------------------------------------------------------- Modal
-const $modal = document.getElementById('modal')
-const $overlay = document.getElementById('overlay')
-const $hideModal = document.getElementById('hide-modal')
+    const $actionContainer = document.querySelector('#action')
+    const $adventureContainer = document.getElementById('#adventure')
+    const $animationContainer = document.getElementById('#animation')
 
-//Busqueda en el espacio previamente mencionado (Modal)
-const $modalTitle = $modal.querySelector('h1')
-const $modalImage = $modal.querySelector('img')
-const $modalDescription = $modal.querySelector('p')
+    const $home = document.getElementById('#home')
+
+    //-------------------------------------------------------- Formulario
+    const $featuringContainer = document.getElementById('#featuring')
+    const $form = document.getElementById('#form')
+
+    //-------------------------------------------------------- Modal
+    const $modal = document.getElementById('modal')
+    const $overlay = document.getElementById('overlay')
+    const $hideModal = document.getElementById('hide-modal')
+
+    //Busqueda en el espacio previamente mencionado (Modal)
+    const $modalTitle = $modal.querySelector('h1')
+    const $modalImage = $modal.querySelector('img')
+    const $modalDescription = $modal.querySelector('p')
+
+
+
+    //console.log(videoItemTemplate('src/images/covers/bitcoin.jpg', 'bitcoin'))
+
+})()
